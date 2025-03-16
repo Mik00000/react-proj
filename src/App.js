afcd9265278c184b0471f9bda889cdd628641188
display-inline-flex <mikjarkov2@gmail.com>
2025-03-16 12:10:42 +0200

    Feture: Header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      Content
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css"
+const Header = () => {
+  return (
+    <header>
+      <h2>Redmonkey</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
